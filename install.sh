164d2a7be41163920e780e29ae974fd66fe13acc
Enric Climnent <enric@ecliment.ovh>
2015-03-15 15:44:44 +0100

    Commented install sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Create .fonts directoy (if does not exist yet)"
 mkdir -p ~/.fonts
+echo "Copy fonts ..."
 cp fonts/* ~/.fonts/
+echo "Update font cache"
 fc-cache -fv ~/.fonts/
